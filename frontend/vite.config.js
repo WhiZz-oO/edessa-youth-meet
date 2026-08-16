0ad47903fbe77db99fb622f32304bb996c620846
WhiZz-oO <whizzoo@github.local>
2026-08-16 22:42:00 +0530

    Add GitHub Actions CI/CD workflow + set Vite base path for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/edessa-youth-meet/',
   plugins: [
     react(),
     tailwindcss()
   ],
 })
+
 
