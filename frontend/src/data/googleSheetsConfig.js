28bf9b4b3f60d3dc5561f638ef4172aef7b78b3b
WhiZz-oO <whizzoo@github.local>
2026-08-17 15:42:12 +0530

    Configure live Google Apps Script Webhook URL for Google Sheets database

--- frontend/src/data/googleSheetsConfig.js
+++ frontend/src/data/googleSheetsConfig.js
@@ -5,5 +5,5 @@ export const GOOGLE_SHEETS_CONFIG = {
   sheetUrl: "https://docs.google.com/spreadsheets/d/1xWZDxn7h01gEOkNMJ_CWoiiSEjY1NKYzXDlsaOYYB1U/edit?usp=sharing",
   sheetId: "1xWZDxn7h01gEOkNMJ_CWoiiSEjY1NKYzXDlsaOYYB1U",
   // Google Apps Script Web App Deployment URL
-  webAppUrl: "https://script.google.com/macros/s/AKfycbz_REPLACE_WITH_YOUR_DEPLOYED_SCRIPT_URL/exec",
+  webAppUrl: "https://script.google.com/macros/s/AKfycbz2dArp8724FJ3kobtwUHQJ7SSRvg5aRPibW_7qhG2eMcjlfH2diKCUC5WHNn59ADVi/exec",
 };
