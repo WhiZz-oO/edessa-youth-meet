46d09210484dba5bc06a72f535d69fd2c5a3f9bf
WhiZz-oO <whizzoo@github.local>
2026-08-17 15:37:44 +0530

    Update Google Sheet database link to new sheet: 1xWZDxn7h01gEOkNMJ_CWoiiSEjY1NKYzXDlsaOYYB1U

--- frontend/src/data/googleSheetsConfig.js
+++ frontend/src/data/googleSheetsConfig.js
@@ -1,9 +1,9 @@
 // Google Sheets Database Configuration
-// Target Google Sheet: https://docs.google.com/spreadsheets/d/1ySJq8CoR03CEnmA-TZ4ZbMUOVAcERMXhj-_EwkEzF7c/edit?usp=sharing
+// Target Google Sheet: https://docs.google.com/spreadsheets/d/1xWZDxn7h01gEOkNMJ_CWoiiSEjY1NKYzXDlsaOYYB1U/edit?usp=sharing
 
 export const GOOGLE_SHEETS_CONFIG = {
-  sheetUrl: "https://docs.google.com/spreadsheets/d/1ySJq8CoR03CEnmA-TZ4ZbMUOVAcERMXhj-_EwkEzF7c/edit?usp=sharing",
-  sheetId: "1ySJq8CoR03CEnmA-TZ4ZbMUOVAcERMXhj-_EwkEzF7c",
+  sheetUrl: "https://docs.google.com/spreadsheets/d/1xWZDxn7h01gEOkNMJ_CWoiiSEjY1NKYzXDlsaOYYB1U/edit?usp=sharing",
+  sheetId: "1xWZDxn7h01gEOkNMJ_CWoiiSEjY1NKYzXDlsaOYYB1U",
   // Google Apps Script Web App Deployment URL
   webAppUrl: "https://script.google.com/macros/s/AKfycbz_REPLACE_WITH_YOUR_DEPLOYED_SCRIPT_URL/exec",
 };
